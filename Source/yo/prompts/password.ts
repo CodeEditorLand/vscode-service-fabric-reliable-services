b81453254650bb2afbf3608744424c2c51f5a300
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-17 16:38:59 +0200

    squash!

--- Source/yo/prompts/password.ts
+++ Source/yo/prompts/password.ts
@@ -1,7 +1,6 @@
-import InputPrompt from './input';
+import InputPrompt from "./input";
 
 export default class PasswordPrompt extends InputPrompt {
-
 	constructor(question: any, answers: any) {
 		super(question, answers);
 
