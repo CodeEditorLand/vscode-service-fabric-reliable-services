db228feeb3987113095e6a31de4b87d9f0e9e16d
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-14 03:14:14 +0300

    squash!

--- test/TestCSharpApplication/build_CodeQL.cmd
+++
@@ -1,2 +0,0 @@
-dotnet restore test/TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -s https://api.nuget.org/v3/index.json
-dotnet build test/TestCSharpApplication/src/TestCSharpApplication/Service1/Service1.csproj -v normal
